3791cfa21ad52c4540642c9fde7dc5df5ca8108e
Arsen4ik <arsenfazleev@gmail.com>
2024-03-22 00:43:20 +0300

    commit 1: загрузка приложения

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GadgetsService } from './service/gadgets.service';
+import { GadgetListComponent } from './components/gadget-list/gadget-list.component';
+import { SearchComponent } from './components/search/search.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GadgetListComponent, SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'cp1';
+  gadgetsService = inject(GadgetsService)
 }
